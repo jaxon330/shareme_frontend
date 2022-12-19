8e8eae6eba2bc4ab601a9c096d2ec3bbd4fa93d0
john-330 <jaxon330@gmail.com>
2022-12-18 23:42:51 -0600

    Pin almost completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -32,7 +32,7 @@ const Feed = () => {
     if(loading) return <Spinner message="We are adding new ideas to your feed!" />
   return (
     <div>
-        Feed
+        {pins && <MasonryLayout pins={pins} />}
     </div>
   )
 }
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -8,13 +8,13 @@ import Pins from './Pins';
 import { userQuery } from '../utils/data';
 import { client } from '../client';
 import logo from '../assets/logo.png'
+import { fetchUser } from '../utils/fetchUser';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser ] = useState(null)
-  
-  const userInfo = localStorage.getItem('user') !== 'underfined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
   const scrollRef = useRef(null)
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
